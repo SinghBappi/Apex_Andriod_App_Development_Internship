29af4f7f5734e867ac8a922233a0afdad673384a
Bappi Singh <bappi.r.singh@gmail.com>
2026-05-03 23:11:01 +0530

    Text Example

--- Good_Practices/app/src/main/java/com/example/hellocompose/MainActivity.kt
+++ Good_Practices/app/src/main/java/com/example/hellocompose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.hellocompose
 
+//import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -16,7 +18,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorProducer
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.shadow.ShadowContext
+//import androidx.compose.ui.platform.getTextLayoutResult
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.hellocompose.ui.theme.HelloComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,12 +44,61 @@ class MainActivity : ComponentActivity() {
             HelloComposeTheme {
                 Surface(color = MaterialTheme.colorScheme.background){
 //                    Mytextview("Hello, World !")
-                    MyTextView("Testing, World !")
+//                    MyTextView("Testing, World !")
+                    TextEx("My name is Bappi Singh and want to become an Developer !");
                 }
+
             }
         }
     }
 }
+@Composable
+fun TextEx(data1:String){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+
+    ) {
+        Text(text = data1,
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
+            color = Color.White,
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            fontFamily = FontFamily.Cursive,
+            letterSpacing = 1.6.sp,
+            textDecoration = TextDecoration.Underline,
+            textAlign = TextAlign.Center,
+            lineHeight = 24.sp,
+            overflow = TextOverflow.Ellipsis,
+            softWrap = true,
+            maxLines = 4,
+            minLines = 1,
+            onTextLayout = {textLayoutResult: TextLayoutResult->
+            val linecount = textLayoutResult.lineCount
+                println("LIne COunt: $linecount")
+            },
+            style = TextStyle(
+                background = Color.Magenta,
+                shadow = Shadow(color = Color.Gray, blurRadius = 40f)
+            )
+
+            )
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
